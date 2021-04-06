cb942995735be1bb8603c3614619ac466a9f9e2d
AlexSandro-P <alex-1175@hotmail.com>
2021-04-06 20:08:36 -0300

    config: setting project configs

---
+++ src/start.py
@@ -0,0 +1,3 @@
+def soma(num_1: int, num_2: int) -> int:
+    soma_dois_numeros = num_1 + num_2
+    return soma_dois_numeros
---
+++ src/start_test.py
@@ -0,0 +1,9 @@
+from .start import soma
+
+
+def test_soma():
+    """ testing soma"""
+
+
+result = soma(2, 4)
+assert result == 6
